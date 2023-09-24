77d72bb8da43b7c6d3c1a5a194633966ad1cd881
Усман <@DESKTOP-E07M5P8>
2023-09-24 15:46:52 +0700

    Add project files.

---
+++ ManageEmployeesDBApplication/Model/DataWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageEmployeesDBApplication.Model
+{
+    internal static class DataWorker
+    {
+        // создать сотрудника
+        // создать департамент
+        // создать позицию
+
+        // удалить сотрудника
+        // удалить департамент
+        // удалить позицию
+
+        // редактировать сотрудника
+        // редактировать департамент
+        // редактировать позицию
+
+    }
+}
