e62af6082da91cf953b80d5ba38a5b4e4b66def1
Усман <@DESKTOP-E07M5P8>
2023-09-26 22:27:41 +0700

    Добавил контекст БД

---
+++ ManageEmployeesDBApplication/Model/Data/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManageEmployeesDBApplication.Model.Data
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=ManageEmployeesDBApplicationDB;Trusted_Connection=True;");
+
+        }
+    }
+}
