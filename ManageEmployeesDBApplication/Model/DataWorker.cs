77f4c263f2b2aa78063b11051abdb987eeed4d4d
Усман <@DESKTOP-E07M5P8>
2023-09-27 01:10:06 +0700

    сделал 2/3 методов редактирования

--- ManageEmployeesDBApplication/Model/DataWorker.cs
+++ ManageEmployeesDBApplication/Model/DataWorker.cs
@@ -3,22 +3,159 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ManageEmployeesDBApplication.Model.Data;
 
 namespace ManageEmployeesDBApplication.Model
 {
     internal static class DataWorker
     {
         // создать сотрудника
+        public static string CreateUser(string name, string surname, string phone, Position position)
+        {
+            string result = "Уже существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isCheckedExist =
+                    db.Users.Any(x => x.Name == name && x.Surname == surname && x.Position == position);
+                if (!isCheckedExist)
+                {
+                    User user = new User()
+                    {
+                        Name = name,
+                        Surname = surname,
+                        Phone = phone,
+                        PositionId = position.Id
+                    };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    result = "Сделано!";
+                }
+                return result;
+            }
+        }
+
         // создать департамент
+        public static string CreateDepartment(string name)
+        {
+            string result = "Уже существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isCheckedExist = db.Departments.Any(x => x.Name == name);
+                if (!isCheckedExist)
+                {
+                    Department department = new Department() { Name = name };
+                    db.Departments.Add(department);
+                    db.SaveChanges();
+                    result = "Сделано!";
+                }
+                return result;
+            }
+        }
+
         // создать позицию
+        public static string CreatePosition(string name, decimal salary, int maxNumber, Department department)
+        {
+            string result = "Уже существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool isCheckedExist = db.Positions.Any(x => x.Name == name && x.Salary == salary);
+                if (!isCheckedExist)
+                {
+                    Position position = new Position()
+                    {
+                        Name = name,
+                        Salary = salary,
+                        MaxNumber = maxNumber,
+                        DepartmentId = department.Id
+                    };
+                    db.Positions.Add(position);
+                    db.SaveChanges();
+                    result = "Сделано!";
+                }
+                return result;
+            }
+        }
 
         // удалить сотрудника
+        public static string DeleteUser(User user)
+        {
+            string result = "Такого сотрудника не существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+                result = "Выполнено! Сотрудник " + user.Name + "уволен.";
+            }
+            return result;
+        }
+
         // удалить департамент
+        public static string DeleteDepartment(Department department)
+        {
+            string result = "Такого департамента не существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Departments.Remove(department);
+                db.SaveChanges();
+                result = "Выполнено!Департамент " + department.Name + "удалён.";
+            }
+            return result;
+        }
+
         // удалить позицию
+        public static string DeletePostion(Position position)
+        {
+            string result = "Такой позиции не существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Positions.Remove(position);
+                db.SaveChanges();
+                result = "Выполнено!Позиция " + position.Name + "удалена.";
+            }
+
+            return result;
+        }
+
 
         // редактировать сотрудника
+
         // редактировать департамент
+        public static string EditDepartment(Department oldDepartment, string newName)
+        {
+            string result = "Такого департамента не существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Department department = db.Departments.FirstOrDefault(d => d.Id == oldDepartment.Id);
+                if (department != null)
+                {
+                    department.Name = newName;
+                    db.SaveChanges();
+                    result = "Выполнено!Департамент " + department.Name + "изменён.";
+                }
+            }
+            return result;
+        }
+
         // редактировать позицию
+        public static string EditPostion(Position oldPosition, string newName, decimal newSalary, int newMaxNumber, Department newDepartment)
+        {
+            string result = "Такой позиции не существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Position position = db.Positions.FirstOrDefault(d => d.Id == oldPosition.Id);
+                if (position != null)
+                {
+                    position.Name = newName;
+                    position.Salary = newSalary;
+                    position.MaxNumber = newMaxNumber;
+                    position.DepartmentId = newDepartment.Id;
+                    db.SaveChanges();
+                    result = "Выполнено!Позиция " + position.Name + "изменена.";
+                }
+            }
+
+            return result;
+        }
 
     }
 }
