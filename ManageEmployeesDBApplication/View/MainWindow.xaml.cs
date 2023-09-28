fe85ecda47a45aacdc4c64373abf2a227052343f
Усман <@DESKTOP-E07M5P8>
2023-09-29 00:22:25 +0700

    привязал команды для добавления

--- ManageEmployeesDBApplication/View/MainWindow.xaml.cs
+++ ManageEmployeesDBApplication/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManageEmployeesDBApplication.ViewModel;
 
 namespace ManageEmployeesDBApplication.View
 {
@@ -23,6 +24,7 @@ namespace ManageEmployeesDBApplication.View
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new DataManageVM();
         }
     }
 }
--- ManageEmployeesDBApplication/ViewModel/DataManageVM.cs
+++ ManageEmployeesDBApplication/ViewModel/DataManageVM.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using ManageEmployeesDBApplication.Model;
+using ManageEmployeesDBApplication.View;
 
 namespace ManageEmployeesDBApplication.ViewModel
 {
@@ -53,6 +56,95 @@ namespace ManageEmployeesDBApplication.ViewModel
             }
         }
 
+        #region COMMANDS TO OPEN WINDOWS
+
+        private RelayCommand openAddNewDepartmentWnd;
+        public RelayCommand OpenAddNewDepartmentWnd
+        {
+            get
+            {
+                return openAddNewDepartmentWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddDepartmentWindowMethod();
+                });
+            }
+        }
+
+        private RelayCommand openAddNewPositionWnd;
+        public RelayCommand OpenAddNewPositionWnd
+        {
+            get
+            {
+                return openAddNewPositionWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddPositionWindowMethod();
+                });
+            }
+        }
+
+        private RelayCommand openAddNewUserWnd;
+        public RelayCommand OpenAddNewUserWnd
+        {
+            get
+            {
+                return openAddNewUserWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddUserWindowMethod();
+                });
+            }
+        }
+
+
+
+        #endregion
+
+
+        #region METHODS TO OPEN WINDOWS
+        //методы открытия окон
+        private void OpenAddDepartmentWindowMethod()
+        {
+            AddNewDepartmentWindow newDepartmentWindow = new AddNewDepartmentWindow();
+            SetCenterPositionAndOpen(newDepartmentWindow);
+        }
+
+        private void OpenAddPositionWindowMethod()
+        {
+            AddNewPositionWindow newPositionWindow = new AddNewPositionWindow();
+            SetCenterPositionAndOpen(newPositionWindow);
+        }
+
+        private void OpenAddUserWindowMethod()
+        {
+            AddNewUserWindow newUserWindow = new AddNewUserWindow();
+            SetCenterPositionAndOpen(newUserWindow);
+        }
+
+        //окна редактирования
+        private void OpenEditDepartmentWindowMethod()
+        {
+            EditDepartmentWindow editDepartmentWindow = new EditDepartmentWindow();
+            SetCenterPositionAndOpen(editDepartmentWindow);
+        }
+
+        private void OpenEditPositionWindowMethod()
+        {
+            EditPositionWindow editPositionWindow = new EditPositionWindow();
+            SetCenterPositionAndOpen(editPositionWindow);
+        }
+
+        private void OpenEditUserWindowMethod()
+        {
+            EditUserWindow editUserWindow = new EditUserWindow();
+            SetCenterPositionAndOpen(editUserWindow);
+        }
+
+        private void SetCenterPositionAndOpen(Window window)
+        {
+            window.Owner = Application.Current.MainWindow;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+        }
+        #endregion
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(string propertyName)
