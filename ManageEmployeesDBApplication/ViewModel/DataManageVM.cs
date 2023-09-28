cf3a6303815765219d290c22e475a2639c90e406
Усман <@DESKTOP-E07M5P8>
2023-09-28 22:24:41 +0700

    Начал работу над DataMangaeVM

--- ManageEmployeesDBApplication/ViewModel/DataManageVM.cs
+++ ManageEmployeesDBApplication/ViewModel/DataManageVM.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ManageEmployeesDBApplication.Model;
 
 namespace ManageEmployeesDBApplication.ViewModel
 {
-    internal class DataManageVM
+    internal class DataManageVM : INotifyPropertyChanged
     {
+        //все отделы
+        private List<Department> allDepartments = DataWorker.GetAllDepartments();
+
+        public List<Department> AllDepartments
+        {
+            get
+            {
+                return allDepartments;
+            }
+            set
+            {
+                allDepartments = value;
+                NotifyPropertyChanged("AllDepartments");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
